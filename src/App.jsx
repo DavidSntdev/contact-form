bcfaa618e825907f8daa68f6db7d35ac6dd8e546
Deivao1337 <contatodeivao@gmail.com>
2024-08-19 02:14:29 -0300

    Adicionado Forms
    adicionado componente do forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import "./App.css";
+import "./styles/App.css";
 import Form from "./components/Form";
 
 function App() {
+  const $btnSubmit = () => {};
   return (
     <>
       <main>
         <h1>Contact US</h1>
         <Form />
+        <button onClick={$btnSubmit}>Submit</button>
       </main>
     </>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,50 @@
-function Form() {}
+import "../styles/Form.css";
+import Type from "./Type";
+
+function Form() {
+  return (
+    <form action="" method="get">
+      <div className="form">
+        <label htmlFor="txtFirstName">First Name</label>
+        <input
+          type="text"
+          name=""
+          id="txtFirstName"
+          autoComplete="on"
+          className="inputInfos"
+        />
+      </div>
+      <div className="form">
+        <label htmlFor="txtLastName">Last Name</label>
+        <input
+          type="text"
+          name=""
+          id="txtLastName"
+          autoComplete="on"
+          className="inputInfos"
+        />
+      </div>
+      <div className="form">
+        <label htmlFor="txtEmail">Email Adress</label>
+        <input
+          type="text"
+          name=""
+          id="txtEmail"
+          autoComplete="on"
+          className="inputInfos"
+        />
+      </div>
+      <Type />
+      <div className="form">
+        <label htmlFor="txtMessage">Message</label>
+        <input type="text" name="" id="txtMessage" />
+      </div>
+      <div id="check">
+        <input type="checkbox" name="" id="checkbox" />
+        <label htmlFor="checkbox" id="lblCheck">I consent to being contacted by the team</label>
+      </div>
+    </form>
+  );
+}
 
 export default Form;
